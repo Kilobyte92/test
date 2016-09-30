b47ad5c91c548a68f5f8c6e2bb71fa1af728fe66
Kilobyte92 <matteo.mini92@gmail.com>
2016-09-30 22:53:56 +0200

    provo a capire come funziona git

---
+++ src/test/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Matteo
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean flag = false;
+        System.out.println(flag);
+    }
+    
+}
