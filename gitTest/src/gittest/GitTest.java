7aa8437f32bdfaf882f1327e18717a239da42ff7
Matteo <Matteo@192.168.1.71>
2016-10-02 01:24:25 +0200

    cerco disperatamente di capire come funziona github

---
+++ gitTest/src/gittest/GitTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author Matteo
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Cerco disperatamente di capire come funziona github");
+    }
+    
+}
